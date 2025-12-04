8ccd54422df2ac7cd3474c2910025035c7bd6bbf
Ingo Wolf <me@ingowolf.au>
2025-12-04 19:34:42 +1100

    add covnex better auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { ConvexReactClient } from "convex/react";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { authClient } from "@/lib/auth-client";
+const convex = new ConvexReactClient(
+  process.env.EXPO_PUBLIC_CONVEX_URL as string,
+  {
+    // Optionally pause queries until the user is authenticated
+    expectAuth: true,
+    unsavedChangesWarning: false,
+  },
+);
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      <Stack />
+    </ConvexBetterAuthProvider>
+  );
 }
