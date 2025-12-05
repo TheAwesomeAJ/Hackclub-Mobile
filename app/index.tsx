d6afc3d6ccbb2aea2e430c6fd4cd34d8cc040aef
Ingo Wolf <me@ingowolf.au>
2025-12-05 14:47:28 +1100

    add showing auth state

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,15 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <AuthLoading>
+        <Text>loading...</Text>
+      </AuthLoading>
+      <Unauthenticated>
+        <Text>Unauthenticated</Text>
+      </Unauthenticated>
+      <Authenticated>
+        <Text>Authenticated</Text>
+      </Authenticated>
     </View>
   );
 }
