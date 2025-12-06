418d36e6a90c7a39e3cbddceb8ac2fefc12f51d8
Ingo Wolf <me@ingowolf.au>
2025-12-07 10:52:41 +1100

    add custom mapping with slack id

--- convex/auth.ts
+++ convex/auth.ts
@@ -49,6 +49,15 @@ export const createAuth = (
             discoveryUrl:
               "https://auth.hackclub.com/.well-known/openid-configuration",
             scopes: ["openid", "profile", "email", "name", "slack_id"],
+            mapProfileToUser: async (profile) => {
+              return {
+                name: profile.given_name,
+                email: profile.email,
+                emailVerified: profile.email_verified,
+                id: profile.sub,
+                slackId: profile.slack_id,
+              };
+            },
           },
         ],
       }),
