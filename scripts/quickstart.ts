dc6f47d3945c30ba59e4cee8d2565e388ea8adfe
Ingo Wolf <me@ingowolf.au>
2025-12-15 16:41:40 +1100

    start working on quickstart script

---
+++ scripts/quickstart.ts
@@ -0,0 +1,32 @@
+import { spawn } from "child_process";
+import readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+function exec(executable: string, args: string[]) {
+  console.log("> " + executable + " " + args.join(" "));
+  return new Promise<void>((resolve, reject) => {
+    const process = spawn(executable, args, {
+      stdio: "inherit",
+    });
+
+    process.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Process exited with code ${code}`));
+      }
+    });
+
+    process.on("error", (error) => {
+      reject(error);
+    });
+  });
+}
+
+(async () => {
+  console.log(`Step 1: Setting up convex
+We recommend using a cloud deployment, but local will work as well
+Follow the prompts from the Convex CLI`);
+
+  await exec("bunx", ["convex", "dev", "--until-success"]);
+})();
