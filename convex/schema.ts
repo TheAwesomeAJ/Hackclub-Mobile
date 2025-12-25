7d7276d5b3a6bf53775733816ca967bda30fd73a
Ingo Wolf <me@ingowolf.au>
2025-12-26 08:02:00 +1100

    add category

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,18 +3,28 @@ import { v } from "convex/values";
 
 export const tables = {
   yswses: defineTable({
-    "name": v.string(),
-    "description": v.string(),
-    "website": v.optional(v.string()),
-    "slack": v.optional(v.string()),
-    "slackChannel": v.string(),
-    "status": v.union(v.literal("active"), v.literal("draft"), v.literal("ended")),
-    "detailedDescription": v.optional(v.string()),
-    "deadline": v.optional(v.string()),
-    "ended": v.optional(v.string()),
-    "participants": v.optional(v.number()),
-    "steps": v.optional(v.array(v.string())),
-    "details": v.optional(v.array(v.string())),
-    "requirements": v.optional(v.union(v.array(v.string()), v.string()))
-  })
-}
+    category: v.union(
+      v.literal("limitedTime"),
+      v.literal("recentlyEnded"),
+      v.literal("indefinite"),
+      v.literal("drafts"),
+    ),
+    name: v.string(),
+    description: v.string(),
+    website: v.optional(v.string()),
+    slack: v.optional(v.string()),
+    slackChannel: v.string(),
+    status: v.union(
+      v.literal("active"),
+      v.literal("draft"),
+      v.literal("ended"),
+    ),
+    detailedDescription: v.optional(v.string()),
+    deadline: v.optional(v.string()),
+    ended: v.optional(v.string()),
+    participants: v.optional(v.number()),
+    steps: v.optional(v.array(v.string())),
+    details: v.optional(v.array(v.string())),
+    requirements: v.optional(v.union(v.array(v.string()), v.string())),
+  }),
+};
