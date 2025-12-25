619f2d41f195f048cc422b1cfd9bcd466bb9b81f
Ingo Wolf <me@ingowolf.au>
2025-12-26 08:09:53 +1100

    move schema to variable to reusability

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,30 +1,28 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
+export const yswsTable = {
+  category: v.union(
+    v.literal("limitedTime"),
+    v.literal("recentlyEnded"),
+    v.literal("indefinite"),
+    v.literal("drafts"),
+  ),
+  name: v.string(),
+  description: v.string(),
+  website: v.optional(v.string()),
+  slack: v.optional(v.string()),
+  slackChannel: v.string(),
+  status: v.union(v.literal("active"), v.literal("draft"), v.literal("ended")),
+  detailedDescription: v.optional(v.string()),
+  deadline: v.optional(v.string()),
+  ended: v.optional(v.string()),
+  participants: v.optional(v.number()),
+  steps: v.optional(v.array(v.string())),
+  details: v.optional(v.array(v.string())),
+  requirements: v.optional(v.union(v.array(v.string()), v.string())),
+};
+
 export default defineSchema({
-  yswses: defineTable({
-    category: v.union(
-      v.literal("limitedTime"),
-      v.literal("recentlyEnded"),
-      v.literal("indefinite"),
-      v.literal("drafts"),
-    ),
-    name: v.string(),
-    description: v.string(),
-    website: v.optional(v.string()),
-    slack: v.optional(v.string()),
-    slackChannel: v.string(),
-    status: v.union(
-      v.literal("active"),
-      v.literal("draft"),
-      v.literal("ended"),
-    ),
-    detailedDescription: v.optional(v.string()),
-    deadline: v.optional(v.string()),
-    ended: v.optional(v.string()),
-    participants: v.optional(v.number()),
-    steps: v.optional(v.array(v.string())),
-    details: v.optional(v.array(v.string())),
-    requirements: v.optional(v.union(v.array(v.string()), v.string())),
-  }),
+  yswses: defineTable(yswsTable),
 });
